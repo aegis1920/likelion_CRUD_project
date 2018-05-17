f9f28eb08a09b9f663cd70fd79d6e4f433ac1fd7
aegis1920 <aegis1920@gmail.com>
2018-05-17 17:29:44 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
 
-  # 처음 '/'의 의미는 URL에서 맨 오른쪽의 최상위 root를 의미한다. 예를 들면, https://wjfijilasdj.io/ 에서 맨 오른쪽.
-  #그냥 root('/')이면 notes controller에 있는 index file을 찾아서 보여준다. 
-  # home 컨트롤러의 index라는 액션을 취해라. 
+  # 처음 '/'의 의미는 URL에서 맨 오른쪽의 생략되어있는 최상위 root를 의미한다. 예를 들면, https://wjfijilasdj.io 에서 맨 오른쪽.
   
   #Create
   # get '/' => 는 root와 같다. 예를 들면 root 'bamboo#new'가 get '/' => 'bamboo#new'와 같다. 
@@ -14,6 +12,7 @@ Rails.application.routes.draw do
   post '/secrets/create' => 'secrets#create' # 저장 페이지
   
   #Read
+  #그냥 root('/')이면 secrets controller에 있는 index file을 찾아서 보여준다.
   get '/' => 'secrets#index' # 전체 목록 보기
   get '/secrets/:id' => 'secrets#show'
   
